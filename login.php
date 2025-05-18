8c1fb27335938c586a88b34c48c1761cfa9de885
MikolajMisiarz <misiarzmikolajtp@gmail.com>
2025-05-18 15:02:52 +0200

    zmieniony formularz logowania

--- login.php
+++ login.php
@@ -11,20 +11,20 @@
 <body>
   <div class="login-container">
     <h2>Zaloguj się</h2>
-    <form action="login.php" method="post">
-      <input type="email" name="email" placeholder="Adres email" required />
-      <input type="password" name="password" placeholder="Hasło" required />
-      
-      <label class="remember-me">
-        <input type="checkbox" name="remember" id="remember">
-        <span>Zapamiętaj mnie</span>
-      </label>
-      
-      <div class="buttons">
-        <button type="submit" class="login-btn">Zaloguj</button>
-        <a href="rejestracja.html" class="register-btn">Zarejestruj się</a>
-      </div>
-    </form>
+    form action="login.php" method="post">
+  <input type="email" name="email" placeholder="Adres email" required />
+  <input type="password" name="password" placeholder="Hasło" required />
+  
+  <label class="remember-me">
+    <input type="checkbox" name="remember" id="remember">
+    <span>Zapamiętaj mnie</span>
+  </label>
+  
+  <div class="buttons">
+    <button type="submit" class="login-btn">Zaloguj</button>
+    <a href="rejestracja.php" class="register-btn">Zarejestruj się</a>
+  </div>
+</form>
     <a href="index.php" class="back-btn">← Powrót do strony głównej</a>
     <?php if (isset($error)): ?>
       <div class="error-message"><?= htmlspecialchars($error) ?></div>
